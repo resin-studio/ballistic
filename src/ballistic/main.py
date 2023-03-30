439712f94ef826d260379a2819bf0c748c84b991
Thomas Logan <thomas.logan.atx@gmail.com>
2023-03-30 12:04:13 -0500

    update

---
+++ src/ballistic/main.py
@@ -0,0 +1,18 @@
+import util
+
+from textx import metamodel_from_file
+parser = metamodel_from_file(util.resource('grammars/bll.tx'))
+
+def parse_from_file(path):
+    return parser.model_from_file(path)
+
+if __name__ == "__main__":
+    print(parse_from_file(util.resource('examples/hello.bll')))
+    # print(f'''
+    # -------------------------
+    # {util.resource("bll.tx")}
+    # -------------------------
+    # {util.project_path()}
+    # -------------------------
+    # ''')
+    # pass
---
+++ src/ballistic/util.py
@@ -0,0 +1,32 @@
+import os
+import pathlib
+
+def project_path():
+    return pathlib.Path(__file__).parent.parent.parent.absolute()
+
+def resource(rel_path : str):
+    base_path = os.path.join(project_path(), 'res')
+    return os.path.abspath(os.path.join(base_path, rel_path))
+
+def all_paths(base_root : str) -> list[str]:
+    paths = []
+    for root,_,files in os.walk(base_root, topdown=True):
+        prefix = (
+            ""
+            if root == base_root else
+	    root[len(base_root) + 1:] + '/'
+        )
+        for f in files:
+            paths.append(prefix + f)
+    return paths
+
+
+def write(dirpath : str, fname : str, code : str, append : bool = False):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+    fpath = os.path.join(dirpath, f"{fname}")
+
+    with open(fpath, 'a' if append else 'w') as f:
+        # logging.info(f"Writing file: {fpath}")
+        f.write(code)
