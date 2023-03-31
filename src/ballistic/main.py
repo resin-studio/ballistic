cb11a644b58ec59a8c4e0b9775536b6a7a665b8f
Thomas Logan <thomas.logan.atx@gmail.com>
2023-03-31 18:53:09 -0500

    update

--- src/ballistic/main.py
+++ src/ballistic/main.py
@@ -1,60 +1,85 @@
 import util
 
+import logging
+import os
+
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pyro
+import pyro.distributions as dist
+import pyro.distributions.constraints as constraints
+
 from textx import metamodel_from_file
 parser = metamodel_from_file(util.resource('grammars/bll.tx'))
 
 def parse_from_file(path):
     return parser.model_from_file(path)
 
-def generate_from_ast(ast):
+def generate_model_from_ast(ast):
     input_name = next(param.name for param in ast.params)
     return f'''
 def model({", ".join([param.name for param in ast.params])}):
-    {generate_from_body(input_name, ast.body)}
+    {generate_model_from_body(input_name, ast.body)}
+
+auto_guide = pyro.infer.autoguide.AutoNormal(model)
+adam = pyro.optim.Adam({{"lr": 0.02}})  # Consider decreasing learning rate.
+elbo = pyro.infer.Trace_ELBO()
+svi = pyro.infer.SVI(model, auto_guide, adam, elbo)
+
+predictive = pyro.infer.Predictive(model, guide=auto_guide, num_samples=100)
+
+def predict({", ".join(param.name for param in ast.params)}):
+    global predictive
+    svi_samples = predictive({", ".join("torch.tensor([" + param.name + "])" for param in ast.params)})
+    svi_gdp = svi_samples["obs"]
+    return svi_gdp[:, 0]
     '''
-#     a = pyro.param("a", lambda: torch.randn(()))
-#     b_a = pyro.param("bA", lambda: torch.randn(()))
-#     b_r = pyro.param("bR", lambda: torch.randn(()))
-#     b_ar = pyro.param("bAR", lambda: torch.randn(()))
-#     sigma = pyro.param("sigma", lambda: torch.ones(()), constraint=constraints.positive)
 
-#     mean = a + b_a * is_cont_africa + b_r * ruggedness + b_ar * is_cont_africa * ruggedness
-
-#     with pyro.plate("data", len(ruggedness)):
-#         return pyro.sample("obs", dist.Normal(mean, sigma), obs=log_gdp)
-
-# def foo(ica, r):
-#     svi_samples = predictive(torch.tensor([ica]), torch.tensor([r]), log_gdp=None)
-#     svi_gdp = svi_samples["obs"]
-#     return svi_gdp[:, 0]
-
-def generate_from_body(input_name, body):
+def generate_model_from_body(input_name, body):
     if body.__class__.__name__ == "Bind":
         return f'''
-    {body.name} = {generate_from_dist(body.name, body.src)}
-    {generate_from_body(input_name, body.dst)}
+    {body.name} = {generate_model_from_dist(body.name, body.src)}
+    {generate_model_from_body(input_name, body.dst)}
         '''
     else: 
         return f'''
     with pyro.plate("data", len({input_name})):
-        return {generate_from_dist("obs", body)}
+        return {generate_model_from_dist("obs", body)}
         '''
 
-def generate_from_dist(name, dist):
+def generate_model_from_dist(name, dist):
     if dist.__class__.__name__ == "Normal":
-        return f'pyro.sample("{name}", dist.Normal({generate_from_expr(dist.mean)}, {generate_from_expr(dist.sigma)}))'
+        return f'pyro.sample("{name}", dist.Normal({generate_model_from_expr(dist.mean)}, {generate_model_from_expr(dist.sigma)}))'
     else:
         assert dist.__class__.__name__ == "Direct"
-        return f'{generate_from_expr(dist.content)}'
+        return f'{generate_model_from_expr(dist.content)}'
 
-def generate_from_expr(expr):
+def generate_model_from_expr(expr):
     return f'{expr}'
 
+def generate_function(file):
+    program_ast = parse_from_file(file)
+    python_str = generate_model_from_ast(program_ast)
+    exec(python_str)
+
+
+    d = {}
+    exec(python_str, globals(), d)
+    return d['predict']
+
+
 if __name__ == "__main__":
-    program = parse_from_file(util.resource('examples/hello.bll'))
-    print('-----------------------------------------------------')
-    print(generate_from_ast(program))
-    print('-----------------------------------------------------')
+    # program = parse_from_file(util.resource('examples/hello.bll'))
+    # print('-----------------------------------------------------')
+    # print(generate_model_from_ast(program))
+    # print('-----------------------------------------------------')
+
+    predict = generate_function(util.resource('examples/hello.bll'))
+    print(predict(5.0))
     # print(f'''
     # -------------------------
     # {util.resource("bll.tx")}
--- src/dev/main.py
+++ src/dev/main.py
@@ -11,7 +11,8 @@ import pyro
 import pyro.distributions as dist
 import pyro.distributions.constraints as constraints
 
-model_str = f'''
+def mything():
+    model_str = f'''
 def model(x):
     
     m = pyro.sample("m", dist.Normal(0.0, 1.0))
@@ -22,7 +23,70 @@ def model(x):
     
     with pyro.plate("data", len(x)):
         return pyro.sample("obs", dist.Normal(z, 1.0))
-'''
 
-(exec(model_str))
-print(model(torch.tensor([5.0])))
+auto_guide = pyro.infer.autoguide.AutoNormal(model)
+adam = pyro.optim.Adam({{"lr": 0.02}})  # Consider decreasing learning rate.
+elbo = pyro.infer.Trace_ELBO()
+svi = pyro.infer.SVI(model, auto_guide, adam, elbo)
+
+predictive = pyro.infer.Predictive(model, guide=auto_guide, num_samples=100)
+# construct a function from scalar to distribution
+def foo(x):
+    global predictive
+    svi_samples = predictive(torch.tensor([x]))
+    svi_gdp = svi_samples["obs"]
+    return svi_gdp[:, 0]
+    '''
+
+    d = {}
+    exec(model_str, globals(), d)
+    foo = d['foo']
+    return foo
+
+
+
+foo = mything()
+print(foo(5.0))
+# print(foo(5.0).mean().item())
+
+# def model(x):
+    
+#     m = pyro.sample("m", dist.Normal(0.0, 1.0))
+    
+#     b = pyro.sample("b", dist.Normal(0.0, 1.0))
+    
+#     z = m*x+b
+    
+#     with pyro.plate("data", len(x)):
+#         return pyro.sample("obs", dist.Normal(z, 1.0))
+
+
+# auto_guide = pyro.infer.autoguide.AutoNormal(model)
+# adam = pyro.optim.Adam({"lr": 0.02})  # Consider decreasing learning rate.
+# elbo = pyro.infer.Trace_ELBO()
+# svi = pyro.infer.SVI(model, auto_guide, adam, elbo)
+
+# # losses = []
+# # for step in range(1000 if not smoke_test else 2):  # Consider running for more steps.
+# #     loss = svi.step(is_cont_africa, ruggedness, log_gdp)
+# #     losses.append(loss)
+# #     if step % 100 == 0:
+# #         logging.info("Elbo loss: {}".format(loss))
+
+# # plt.figure(figsize=(5, 2))
+# # plt.plot(losses)
+# # plt.xlabel("SVI step")
+# # plt.ylabel("ELBO loss");
+
+# predictive = pyro.infer.Predictive(model, guide=auto_guide, num_samples=100)
+# # construct a function from scalar to distribution
+# def foo(x):
+#     svi_samples = predictive(torch.tensor([x]))
+#     svi_gdp = svi_samples["obs"]
+#     return svi_gdp[:, 0]
+
+# print(foo(5.0))
+# print(foo(5.0).mean().item())
+
+
+# print(model(torch.tensor([5.0])))
