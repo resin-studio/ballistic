befba3f7b457f7190dd8c5b9c224045508e64bc0
Thomas Logan <thomas.logan.atx@gmail.com>
2023-04-15 12:11:46 -0500

    update

--- src/test_unemploy.py
+++ src/test_unemploy.py
@@ -31,25 +31,38 @@ if __name__ == "__main__":
     DATA_URL = util.resource("data/unemployment_claims.csv")
     df = pd.read_csv(DATA_URL, encoding="ISO-8859-1")
     df = df[df["County/Area"] == "WASHINGTON STATE"][df["Claim Type"] == "Initial claims"]
-    df = df[["Date", "Claims"]]
-    # df = df.iloc[0:4,:]
     df["Date"] = df["Date"].map(lambda s : d(s).toordinal())
-    df["Claims"] = df["Claims"].map(lambda s : float(s))
     df = df[df["Date"] >= d('6/1/08').toordinal()][df["Date"] < d('6/1/19').toordinal()]
-    # df = df.reset_index()
+    df["Date"] = df["Date"].map(lambda n : n - d('6/1/08').toordinal())
+    df["Claims"] = df["Claims"].map(lambda s : float(s))
+    df = df[["Date", "Claims"]]
 
-    # print(df.head(100))
-
-    print(df.values)
+    # df = df.iloc[0:4,:]
+    # print(df.head(4))
+    # print(df.values)
 
     data_train = torch.tensor(df.values, dtype=torch.float)
+
+    #############################
+    result = prog.generate_function(util.resource('examples/unemploy.bll'), data_train)
+    #############################
+
+
+    # print(data_train)
     time_data = data_train[:,0]
     claim_data = data_train[:,1]
+
+    prediction = result.multi(time_data)
+    prediction_mean = prediction.mean(0).detach().cpu().numpy() 
+    prediction_lower = prediction.kthvalue(int(len(prediction) * 0.05), dim=0)[0].detach().cpu.numpy()
+    prediction_upper = prediction.kthvalue(int(len(prediction) * 0.95), dim=0)[0].detach().cpu.numpy()
 
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 6), sharey=True)
     fig.suptitle("Unemployment claims over time", fontsize=16)
 
-    ax.plot(claim_data, "x")
+    ax.plot(time_data, prediction_mean)
+    ax.fill_between(time_data, prediction_lower, prediction_upper, alpha=0.5)
+    ax.plot(time_data, claim_data, "x")
     ax.set(xlabel="Time", ylabel="Unemployment claims", title="Washington State")
 
     plt.show()
