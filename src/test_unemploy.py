c963f376bfda542f7bae15149fcd5f4509becbd7
Thomas Logan <thomas.logan.atx@gmail.com>
2023-04-21 14:45:14 -0500

    update

--- src/test_unemploy.py
+++ src/test_unemploy.py
@@ -24,61 +24,55 @@ from datetime import datetime, date
 def d(s):
     return (datetime.strptime(s, '%m/%d/%y')).date()
 
-if __name__ == "__main__":
-    ##############################
-    ## print('-----------------------------------------------------')
-    ## program = prog.parse_from_file(util.resource('examples/unemploy.bll'))
-    ## print(program)
-    ## print('-----------------------------------------------------')
-    ## print(prog.generate_model_from_ast(program))
-    ## print('-----------------------------------------------------')
-    ## print(d('1/1/05').toordinal())
-    ## print(d('1/2/05').toordinal())
-    ## df = df.iloc[0:4,:]
-    ## print(df.head(4))
-    ## print(df.values)
-    ##############################
+##############################
+## print('-----------------------------------------------------')
+## program = prog.parse_from_file(util.resource('examples/unemploy.bll'))
+## print(program)
+## print('-----------------------------------------------------')
+## print(prog.generate_model_from_ast(program))
+## print('-----------------------------------------------------')
+## print(d('1/1/05').toordinal())
+## print(d('1/2/05').toordinal())
+## df = df.iloc[0:4,:]
+## print(df.head(4))
+## print(df.values)
+##############################
 
-    DATA_URL = util.resource("data/unemployment_claims.csv")
-    df = pd.read_csv(DATA_URL, encoding="ISO-8859-1")
-    df = df[df["County/Area"] == "WASHINGTON STATE"][df["Claim Type"] == "Initial claims"]
-    df["Date"] = df["Date"].map(lambda s : d(s).toordinal())
-    df = df[df["Date"] >= d('1/1/08').toordinal()][df["Date"] < d('11/1/19').toordinal()]
-    df["Claims"] = df["Claims"].map(lambda s : float(s))
-    df = df[np.isfinite(df["Claims"])]
-    # df["Claims"] = np.log(df["Claims"])
-    # df["Date"] = np.log(df["Date"])
-    # df = df[["Date", "Claims"]]
-    # df["Date"] = df["Date"].map(lambda n : n - d('6/1/08').toordinal())
-    df = (df.reset_index().reset_index()[["level_0", "Claims"]])
+DATA_URL = util.resource("data/unemployment_claims.csv")
+df = pd.read_csv(DATA_URL, encoding="ISO-8859-1")
+df = df[df["County/Area"] == "WASHINGTON STATE"][df["Claim Type"] == "Initial claims"]
+df["Date"] = df["Date"].map(lambda s : d(s).toordinal())
+
+df = df[df["Date"] >= d('10/1/08').toordinal()][df["Date"] < d('11/1/19').toordinal()]
+
+df["Claims"] = df["Claims"].map(lambda s : float(s))
+df = df[np.isfinite(df["Claims"])]
+df["Claims"] = np.log(df["Claims"])
+# df["Date"] = np.log(df["Date"])
+# df = df[["Date", "Claims"]]
+# df["Date"] = df["Date"].map(lambda n : n - d('6/1/08').toordinal())
+df = (df.reset_index().reset_index()[["level_0", "Claims"]])
 
 
-    data = torch.tensor(df.values, dtype=torch.float)
-    #########################
-    # result = prog.generate_function(util.resource('examples/unemploy.bll'), data)
+data = torch.tensor(df.values, dtype=torch.float)
+###########################
+#### Generated Program ####
+# result = prog.generate_function(util.resource('examples/unemploy.bll'), data)
+###########################
 
-    import math
+import math
+
+def test_simple_prediction():
+
     def model(month, obs=None):
         slope = pyro.param("slope", torch.tensor(0.))
-        # slope = pyro.param("slope", lambda: torch.randn(()))
-        intercept = pyro.param("intercept", torch.tensor(13.))
-        # intercept = pyro.param("intercept", lambda: torch.randn(()))
-        scale = 1.
-        # sigma = pyro.param("sigma", lambda: torch.ones(()))
+        intercept = pyro.param("intercept", torch.tensor(0.))
+        scale = 0.1
+        # scale = pyro.param("scale", torch.tensor(0.1))
+        # scale = pyro.param("scale", lambda: torch.ones(()))
         
         with pyro.plate("data", len(month)):
             return pyro.sample("obs", dist.LogNormal(slope * month + intercept, scale), obs=obs)
-
-    # def model(month, obs=None):
-    #     slope = pyro.sample("slope", dist.Normal(0.0, 1.0))
-    #     sd = pyro.sample("sd", dist.HalfNormal(1.0))
-    #     with pyro.plate("ms_plate", 12):
-    #         ms = pyro.sample("ms", dist.Normal(0.0, 1.0))
-
-    #     sms = ms - ms.mean()
-        
-    #     with pyro.plate("data", len(month)):
-    #         return pyro.sample("obs", dist.LogNormal(slope * month + sms.repeat(math.ceil(len(month)/len(sms)))[:len(month)], sd), obs=obs)
         
     auto_guide = pyro.infer.autoguide.AutoNormal(model)
     adam = pyro.optim.Adam({"lr": 0.01})  # Consider decreasing learning rate.
@@ -91,28 +85,32 @@ if __name__ == "__main__":
     for step in range(2000 if not smoke_test else 2):  # Consider running for more steps.
         loss = svi.step(data[:, 0], data[:, 1])
         losses.append(loss)
-            
 
-    predictive = pyro.infer.Predictive(model, guide=auto_guide, num_samples=3000)
+    predictive = pyro.infer.Predictive(model, guide=auto_guide, num_samples=500)
 
     def multi(month):
-        global predictive 
+        nonlocal predictive 
         svi_samples = predictive(month)
         svi_obs = svi_samples["obs"]
         return svi_obs
 
     def single(month):
-        global predictive 
+        nonlocal predictive 
         svi_samples = predictive(torch.tensor([month]))
         svi_obs = svi_samples["obs"]
         return svi_obs[:, 0]
+
+
     #########################
 
+
+    # print(data[:,1])
+
     prediction = multi(data[:,0])
+
     # prediction = result.multi(time_data)
     prediction_mean = prediction.mean(0).detach().cpu().numpy() 
     print(prediction_mean)
-    print(data[:,1])
     # prediction_lower = prediction.kthvalue(int(len(prediction) * 0.05), dim=0)[0].detach().cpu.numpy()
     # prediction_upper = prediction.kthvalue(int(len(prediction) * 0.95), dim=0)[0].detach().cpu.numpy()
 
@@ -123,5 +121,119 @@ if __name__ == "__main__":
     # ax.fill_between(time_data, prediction_lower, prediction_upper, alpha=0.5)
     ax.plot(data[:,0], data[:,1], "x")
     ax.set(xlabel="Time", ylabel="Unemployment claims", title="Washington State")
+    ###################################
 
     plt.show()
+
+
+def test_slope():
+
+    def model(month, obs=None):
+        slope = pyro.sample("slope", dist.Normal(0.0, 1.0))
+        scale = pyro.sample("sd", dist.HalfNormal(1.0))
+        # sd = 0.1
+        with pyro.plate("ms_plate", 12):
+            ms = pyro.sample("ms", dist.Normal(0.0, 1.0))
+            sms = ms - ms.mean()
+        
+        with pyro.plate("data", len(month)):
+            intercept = sms.repeat(math.ceil(len(month)/len(sms)))[:len(month)]
+            return pyro.sample("obs", dist.LogNormal(slope * month + intercept, scale), obs=obs)
+        
+
+        
+    auto_guide = pyro.infer.autoguide.AutoNormal(model)
+    adam = pyro.optim.Adam({"lr": 0.01})  # Consider decreasing learning rate.
+    elbo = pyro.infer.Trace_ELBO()
+    svi = pyro.infer.SVI(model, auto_guide, adam, elbo)
+
+
+    smoke_test = ('CI' in os.environ)
+    losses = []
+    for step in range(2000 if not smoke_test else 2):  # Consider running for more steps.
+        loss = svi.step(data[:, 0], data[:, 1])
+        losses.append(loss)
+
+    #########################
+    with pyro.plate("samples", 800, dim=-2):
+        samples = auto_guide(data[:,0])
+
+    slope_samples = samples["slope"]
+
+    fig = plt.figure(figsize=(10, 6))
+    sns.histplot(slope_samples.detach().cpu().numpy(), kde=True, stat="density", label="Slope")
+    fig.suptitle("Density of Slope");
+    plt.xlabel("Slope of regression line")
+    plt.legend()
+    plt.show()
+        
+# ###################################
+def test_prediction():
+
+    def model(month, obs=None):
+        slope = pyro.sample("slope", dist.Normal(0.0, 1.0))
+        scale = pyro.sample("sd", dist.HalfNormal(1.0))
+        # sd = 0.1
+        with pyro.plate("ms_plate", 12):
+            ms = pyro.sample("ms", dist.Normal(0.0, 1.0))
+            sms = ms - ms.mean()
+        
+        with pyro.plate("data", len(month)):
+            intercept = sms.repeat(math.ceil(len(month)/len(sms)))[:len(month)]
+            return pyro.sample("obs", dist.LogNormal(slope * month + intercept, scale), obs=obs)
+        
+
+        
+    auto_guide = pyro.infer.autoguide.AutoNormal(model)
+    adam = pyro.optim.Adam({"lr": 0.01})  # Consider decreasing learning rate.
+    elbo = pyro.infer.Trace_ELBO()
+    svi = pyro.infer.SVI(model, auto_guide, adam, elbo)
+
+
+    smoke_test = ('CI' in os.environ)
+    losses = []
+    for step in range(2000 if not smoke_test else 2):  # Consider running for more steps.
+        loss = svi.step(data[:, 0], data[:, 1])
+        losses.append(loss)
+
+    predictive = pyro.infer.Predictive(model, guide=auto_guide, num_samples=500)
+
+    def multi(month):
+        nonlocal predictive 
+        svi_samples = predictive(month)
+        svi_obs = svi_samples["obs"]
+        return svi_obs
+
+    def single(month):
+        nonlocal predictive 
+        svi_samples = predictive(torch.tensor([month]))
+        svi_obs = svi_samples["obs"]
+        return svi_obs[:, 0]
+
+
+    #########################
+
+
+    # print(data[:,1])
+
+    prediction = multi(data[:,0])
+
+    # prediction = result.multi(time_data)
+    prediction_mean = prediction.mean(0).detach().cpu().numpy() 
+    print(prediction_mean)
+    # prediction_lower = prediction.kthvalue(int(len(prediction) * 0.05), dim=0)[0].detach().cpu.numpy()
+    # prediction_upper = prediction.kthvalue(int(len(prediction) * 0.95), dim=0)[0].detach().cpu.numpy()
+
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 6), sharey=True)
+    fig.suptitle("Unemployment claims over time", fontsize=16)
+
+    ax.plot(data[:,0], prediction_mean)
+    # ax.fill_between(time_data, prediction_lower, prediction_upper, alpha=0.5)
+    ax.plot(data[:,0], data[:,1], "x")
+    ax.set(xlabel="Time", ylabel="Unemployment claims", title="Washington State")
+    ###################################
+
+    plt.show()
+
+test_slope()
+
