24ad2250b7e4e5b9cc57dd1de0fd705854d9049f
Thomas Logan <thomas.logan.atx@gmail.com>
2023-04-23 12:50:36 -0500

    update

--- src/ballistic/prog.py
+++ src/ballistic/prog.py
@@ -26,7 +26,7 @@ parser = metamodel_from_file(util.resource('grammars/bll.tx'))
 def parse_from_file(path):
     return parser.model_from_file(path)
 
-def generate_model_from_ast(ast):
+def compile_model_from_ast(ast):
     input_name = next(param.name for param in ast.params)
 
     training = ''
@@ -48,7 +48,7 @@ for step in range(1000 if not smoke_test else 2):  # Consider running for more s
 
 #     return f'''
 # def model({", ".join([param.name for param in ast.params])}, obs=None):
-#     {generate_model_from_body(input_name, ast.body)}
+#     {compile_model_from_body(input_name, ast.body)}
 
 # auto_guide = pyro.infer.autoguide.AutoNormal(model)
 # adam = pyro.optim.Adam({{"lr": 0.02}})  # Consider decreasing learning rate.
@@ -62,7 +62,7 @@ for step in range(1000 if not smoke_test else 2):  # Consider running for more s
     
     return f'''
 def model({", ".join([param.name for param in ast.params])}, obs=None):
-    {generate_model_from_body(input_name, ast.body)}
+    {compile_model_from_body(input_name, ast.body)}
 
 from pyro.infer.autoguide import AutoMultivariateNormal, init_to_mean
 auto_guide = AutoMultivariateNormal(model, init_loc_fn=init_to_mean)
@@ -89,63 +89,63 @@ def single({", ".join(param.name for param in ast.params)}):
     return svi_obs[:, 0]
     '''
 
-def generate_model_from_body(input_name, body):
+def compile_model_from_body(input_name, body):
     if body.__class__.__name__ == "Sample":
         return f'''
-    {body.name} = {generate_model_from_dist(body.name, body.src)}
-    {generate_model_from_body(input_name, body.contin)}
+    {body.name} = {compile_model_from_dist(body.name, body.src)}
+    {compile_model_from_body(input_name, body.contin)}
         '''
 
     elif body.__class__.__name__ == "Plate":
         return f'''
     with pyro.plate("{body.name}_plate", {body.size}):
-        {body.name} = {generate_model_from_dist(body.name, body.src)}
-    {generate_model_from_body(input_name, body.contin)}
+        {body.name} = {compile_model_from_dist(body.name, body.src)}
+    {compile_model_from_body(input_name, body.contin)}
         '''
     else: 
         return f'''
     with pyro.plate("data", len({input_name})):
-        return {generate_model_from_dist("obs", body)}
+        return {compile_model_from_dist("obs", body)}
         '''
 
-def generate_model_from_dist(name, dist):
+def compile_model_from_dist(name, dist):
     obs_str = ', obs=obs' if name == "obs" else '' 
     if dist.__class__.__name__ == "Normal":
-        return f'pyro.sample("{name}", dist.Normal({generate_model_from_expr(dist.mean)}, {generate_model_from_expr(dist.sigma)}){obs_str})'
+        return f'pyro.sample("{name}", dist.Normal({compile_model_from_expr(dist.mean)}, {compile_model_from_expr(dist.sigma)}){obs_str})'
     elif dist.__class__.__name__ == "Lognorm":
-        return f'pyro.sample("{name}", dist.LogNormal({generate_model_from_expr(dist.mean)}, {generate_model_from_expr(dist.sigma)}){obs_str})'
+        return f'pyro.sample("{name}", dist.LogNormal({compile_model_from_expr(dist.mean)}, {compile_model_from_expr(dist.sigma)}){obs_str})'
     elif dist.__class__.__name__ == "Uniform":
-        return f'pyro.sample("{name}", dist.Uniform({generate_model_from_expr(dist.mean)}, {generate_model_from_expr(dist.sigma)}){obs_str})'
+        return f'pyro.sample("{name}", dist.Uniform({compile_model_from_expr(dist.mean)}, {compile_model_from_expr(dist.sigma)}){obs_str})'
     elif dist.__class__.__name__ == "Halfnorm":
-        return f'pyro.sample("{name}", dist.HalfNormal({generate_model_from_expr(dist.scale)}){obs_str})'
+        return f'pyro.sample("{name}", dist.HalfNormal({compile_model_from_expr(dist.scale)}){obs_str})'
     else:
         assert dist.__class__.__name__ == "Direct"
-        return f'{generate_model_from_expr(dist.content)}'
+        return f'{compile_model_from_expr(dist.content)}'
 
-def generate_model_from_expr(expr):
-    base_str = generate_model_from_prod(expr.base)
+def compile_model_from_expr(expr):
+    base_str = compile_model_from_prod(expr.base)
     exts_str = ''
     for ext in expr.exts:
-        exts_str += (f' {ext.op} ' + generate_model_from_prod(ext.arg))
+        exts_str += (f' {ext.op} ' + compile_model_from_prod(ext.arg))
         
     return base_str + exts_str 
 
-def generate_model_from_prod(prod):
-    base_str = generate_model_from_atom(prod.base)
+def compile_model_from_prod(prod):
+    base_str = compile_model_from_atom(prod.base)
     factors_str = ''
     for factor in prod.factors:
-        factors_str += (f' {factor.op} ' + generate_model_from_atom(factor.arg))
+        factors_str += (f' {factor.op} ' + compile_model_from_atom(factor.arg))
         
     return base_str + factors_str 
 
-def generate_model_from_atom(atom):
+def compile_model_from_atom(atom):
     if atom.__class__.__name__ == "Paren":
-        return '(' + generate_model_from_expr(atom.content) + ')'
+        return '(' + compile_model_from_expr(atom.content) + ')'
     elif atom.__class__.__name__ == "Mean":
-        return generate_model_from_expr(atom.vector) + '.mean()'
+        return compile_model_from_expr(atom.vector) + '.mean()'
     elif atom.__class__.__name__ == "Project":
-        v = generate_model_from_expr(atom.vector)
-        i = generate_model_from_expr(atom.index)
+        v = compile_model_from_expr(atom.vector)
+        i = compile_model_from_expr(atom.index)
         return f'{v}.repeat(math.ceil(len({i})/len({v})))[:len({i})]'
     else:
         return f'{atom}'
@@ -156,13 +156,15 @@ class Stoch:
     single : Callable 
 
 
-def generate_function(file, data=None):
-    program_ast = parse_from_file(file)
-    python_str = generate_model_from_ast(program_ast)
-    print('--- Generated Python: Start ---')
+def learn_posteriors(program_ast, data):
+    python_str = compile_model_from_ast(program_ast)
+    print('--- compiled Python: Start ---')
     print(python_str)
-    print('--- Generated Python: End -----')
+    print('--- compiled Python: End -----')
     d = {'data' : data}
     exec(python_str, globals(), d)
     return Stoch(multi = d['multi'], single=d['single'])
 
+def generate_function(file, data=None):
+    program_ast = parse_from_file(file)
+    return learn_posteriors(program_ast, data)
